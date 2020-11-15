83eb8f8ffeacb8b4540a1a10e2175659532b3f3b
lalitggn <lalitggn@yahoo.com>
2020-11-15 20:11:54 +0000

    Adding new Line in App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hellow123 World!!!!" );
+        System.out.println("Hellow123 World!!!!" );
+	System.out.println("This is next line in Hello World!");
     }
 }
